16710c77359fcfa863d250a87ca13648ffdfd811
Jahid <63216540+JahidWAV@users.noreply.github.com>
2026-09-10 14:27:53 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -63,6 +63,13 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|auth/callback).*)',
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, auth/callback
+     * - Any file with an extension (e.g. .svg, .png, etc.)
+     */
+    '/((?!_next/static|_next/image|favicon.ico|auth/callback|.*\\..*).*)',
   ],
 }
