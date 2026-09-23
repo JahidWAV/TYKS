cac2972a25bff830ac0bac0ff25f939b770177d7
Jahid <63216540+JahidWAV@users.noreply.github.com>
2026-09-23 05:15:38 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -66,7 +66,7 @@ export default function Navbar({ isPro = false, isDarkMode = false }: NavbarProp
       <div className="absolute top-6 left-0 right-0 z-50 max-w-7xl mx-auto px-6 lg:px-12 font-sans">
         <header className="w-full flex items-center justify-between gap-4">
 
-          {/* 1. BLOC GAUCHE (Logo TYKS agrandi) */}
+          {/* 1. BLOC GAUCHE (Logo TYKS style DICE - Large et impactant) */}
           <div className="flex items-center justify-start">
             <Link 
               href="/" 
@@ -75,10 +75,10 @@ export default function Navbar({ isPro = false, isDarkMode = false }: NavbarProp
               <Image 
                 src="/logo.svg" 
                 alt="TYKS" 
-                width={480} 
-                height={160} 
+                width={500} 
+                height={140} 
                 priority 
-                className="h-20 sm:h-24 md:h-28 w-auto object-contain transition-all duration-300" 
+                className="h-14 sm:h-18 md:h-22 w-auto object-contain transition-all duration-300 scale-110 origin-left" 
               />
             </Link>
           </div>
